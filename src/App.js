22aa308ce703bebad86e5534ca4164927b4b7669
rajnish1999 <rajnishtiwarihrt@gmail.com>
2021-08-12 10:24:30 +0530

    Added Header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="app">
-      <h1> rajnish tiwari</h1>
+      <Header />
     </div>
   );
 }
